f77da6b84005173a796af5728d06346897ec4205
fernandoberbel <fberbel1000@gmail.com>
2024-04-30 21:41:26 -0300

    feat: add category item and list components

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,17 @@
+import { db } from "../_lib/prisma";
+import CategoryItem from "./category-item";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+  // pegar as categorias do banco de dados
+  // renderizar um item pra cada categoria
+  return (
+    <div className="flex gap-3 overflow-scroll">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
