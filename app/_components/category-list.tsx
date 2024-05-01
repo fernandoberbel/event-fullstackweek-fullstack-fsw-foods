06fda78ba07e03278587a4e60c5ce6aab9810703
fernandoberbel <fberbel1000@gmail.com>
2024-04-30 22:26:33 -0300

    fix: layout category

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -6,7 +6,7 @@ const CategoryList = async () => {
   // pegar as categorias do banco de dados
   // renderizar um item pra cada categoria
   return (
-    <div className="flex gap-3 overflow-scroll">
+    <div className="grid grid-cols-2 gap-3">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
